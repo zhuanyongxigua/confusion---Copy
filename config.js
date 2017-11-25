06e28d2f7b2f0c10be6b7eb6489873e6edd062bb
yuxin <yx18513839223@gmail.com>
2017-11-25 15:04:02 +0800

    reset

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://47.93.201.67:27018/db',
+    'mongoUrl': 'mongodb://localhost:27017/db',
     'facebook': {
         clientID: '1762929944025478',
         clientSecret: 'f5099dd50987c1be45157c73ca6a36e1',
