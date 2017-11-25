e1bb89ac5e5ae2e7ce8a300aa40d0ab3bf66bdf5
yuxin <yx18513839223@gmail.com>
2017-11-25 10:56:44 +0800

    modify the mongodb connection ip

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'secretKey': '12345-67890-09876-54321',
-  'mongoUrl':'mongodb://localhost:27017/db',
-  'facebook': {
-    clientID: '1762929944025478',
-    clientSecret: 'f5099dd50987c1be45157c73ca6a36e1',
-    callbackURL: 'https://localhost:3443/users/facebook/callback'
-  }
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://47.93.201.67:27017/db',
+    'facebook': {
+        clientID: '1762929944025478',
+        clientSecret: 'f5099dd50987c1be45157c73ca6a36e1',
+        callbackURL: 'https://47.93.201.67:3443/users/facebook/callback'
+    }
 }
